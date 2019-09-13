4b674b9449b825f10036086cf47a249939ce2188
andrew escobedo <aevistax@gmail.com>
2019-09-13 12:39:46 -0500

    changed ordering of type param in control api

--- src/main/scala/com/network/control/Control.scala
+++ src/main/scala/com/network/control/Control.scala
@@ -1,25 +1,25 @@
 package com.network.control
 
 object Control {
-  def apply[A](): Control[A ,A] =
+  def apply[A](): Control[A, A] =
     new Control[A, A](a => (cb: A => Unit) => cb(a))
 
-  def process[A, V](a: => A): Control[A, V] =
-    new Control[A, V](_ => (cb: A => Unit) => cb(a))
+  def process[V, A](a: => A): Control[V, A] =
+    new Control[V, A](_ => (cb: A => Unit) => cb(a))
 }
 
-class Control[A, V] private(private val control: V => (A => Unit) => Unit) {
+class Control[V, A] private(private val control: V => (A => Unit) => Unit) {
 
-  final def map[B](f: A => B): Control[B, V] =
+  final def map[B](f: A => B): Control[V, B] =
     flatMap(a => Control.process(f(a)))
 
-  final def flatMap[B](f: A => Control[B, V]): Control[B, V] =
-    new Control[B, V](v => cb => control(v){a => f(a).control(v)(cb)})
+  final def flatMap[B](f: A => Control[V, B]): Control[V, B] =
+    new Control[V, B](v => cb => control(v){a => f(a).control(v)(cb)})
 
-  final def filter(f: A => Boolean): Control[A, V] =
-    new Control[A, V](v => cb => control(v){a => if (f(a)) cb(a)})
+  final def filter(f: A => Boolean): Control[V, A] =
+    new Control[V, A](v => cb => control(v){a => if (f(a)) cb(a)})
 
-  final def andThen[B](f: A => B): Control[A, V] =
+  final def andThen[B](f: A => B): Control[V, A] =
     map(a => { f(a); a })
 
   final def process(v: V): Unit =
